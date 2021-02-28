df593043305dede4ab04c248ebb5b76390276186
Alena Kudasheva <alenka_kudasheva@mail.ru>
2021-02-28 23:00:02 +0300

    edited god's actions

--- main_game/include/God.h
+++ main_game/include/God.h
@@ -11,16 +11,15 @@ public:
 static void show_menu();
 static void close_menu();
 static void show_game_field();
-static void close_game_field();
+static void set_object(int x, int y, int size, const std::string& hash, const std::string& object_name);
+static void move_object(int x, int y, const std::string& hash);
+static void delete_object(const std::string& hash);
 
 //for logic
 static void clicked_on_start();
 static void clicked_on_exit();
 static void pushed_button_left();
 static void pushed_button_right();
-static void set_object(int x, int y, int size, const std::string& hash, const std::string& object_name);
-static void move_object(int x, int y, const std::string& hash);
-static void delete_object(const std::string& hash);
 
 //both
 
