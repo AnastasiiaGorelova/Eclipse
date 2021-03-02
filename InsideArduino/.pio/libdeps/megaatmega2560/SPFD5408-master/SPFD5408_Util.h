d4341a024388f5f3e0885944469273685e39e27a
Frosendroska <katyabraun0@gmail.com>
2021-03-02 13:07:25 +0300

    InsideArduino

--- InsideArduino/.pio/libdeps/megaatmega2560/SPFD5408-master/SPFD5408_Util.h
+++
@@ -1,9 +0,0 @@
-///////////////////////////////////
-// Library: SPFD5408 Library
-// File: SPFD5408_Util.h
-///////////////////////////////////
-
-#ifndef _SPFD5408_UTIL_H_
-#define _SPFD5408_UTIL_H_
-
-#endif
