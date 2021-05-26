d3761ee2f5dbf08bc3a3ac3695ae8aeed401fda4
Frosendroska <katyabraun0@gmail.com>
2021-05-26 14:16:57 +0300

    For server

--- main_game/src_upstream_leaderboard/server.cpp
+++ main_game/src_upstream_leaderboard/server.cpp
@@ -1,43 +1,43 @@
-//#include <boost/asio.hpp>
-//#include <cassert>
-//#include <cstdlib>
-//#include <exception>
-//#include <iostream>
-//#include <sstream>
-//#include <utility>
-//
-//using boost::asio::ip::tcp;
-//
-//int main(int argc, char *argv[]) {
-//    {
-//        argc = 2;
-//        argv[1] = "23";
-//    }
-//    assert(argc == 2);
-//
-//    boost::asio::io_context io_context;
-//
-//    // описание сокета на котором нужно начать слушать
-//    tcp::acceptor acceptor(io_context,
-//                           tcp::endpoint(tcp::v4(), std::atoi(argv[1])));
-//    // можно узнать на каком порту слушает
-//    std::cout << "Listening at " << acceptor.local_endpoint() << "\n";
-//
-//    tcp::iostream client([&]() {
-//        // блокируется пока не подключится новый клиент
-//        tcp::socket s = acceptor.accept();
-//        // откуда куда
-//        std::cout << "Connected " << s.remote_endpoint() << " --> "
-//                  << s.local_endpoint() << "\n";
-//        return s;
-//    }());
-//
-//    // пока активен
-//    while (client) {
-//        std::string s;
-//        client >> s;
-//        client << "string: " << s << "\n";
-//    }
-//    std::cout << "Completed\n";
-//    return 0;
-//}
+#include <boost/asio.hpp>
+#include <cassert>
+#include <cstdlib>
+#include <exception>
+#include <iostream>
+#include <sstream>
+#include <utility>
+
+using boost::asio::ip::tcp;
+
+int main(int argc, char *argv[]) {
+    {
+        argc = 2;
+        argv[1] = "23";
+    }
+    assert(argc == 2);
+
+    boost::asio::io_context io_context;
+
+    // описание сокета на котором нужно начать слушать
+    tcp::acceptor acceptor(io_context,
+                           tcp::endpoint(tcp::v4(), std::atoi(argv[1])));
+    // можно узнать на каком порту слушает
+    std::cout << "Listening at " << acceptor.local_endpoint() << "\n";
+
+    tcp::iostream client([&]() {
+        // блокируется пока не подключится новый клиент
+        tcp::socket s = acceptor.accept();
+        // откуда куда
+        std::cout << "Connected " << s.remote_endpoint() << " --> "
+                  << s.local_endpoint() << "\n";
+        return s;
+    }());
+
+    // пока активен
+    while (client) {
+        std::string s;
+        client >> s;
+        client << "string: " << s << "\n";
+    }
+    std::cout << "Completed\n";
+    return 0;
+}
