8e4c08ceca819b3ad8120fd61e9fb394aafb3452
AnastasiiaGorelova <anastasia.gorelova2003@gmail.com>
2021-05-28 17:38:42 +0300

    with correct python version

--- main_game/upstream_leaderboard/upstream_leaderboard.cpp
+++ main_game/upstream_leaderboard/upstream_leaderboard.cpp
@@ -1,7 +1,7 @@
 #include "upstream_leaderboard.h"
 
 void UpstreamLeaderboard::call_client(const Player &cur_player) {
-    std::string string_req = "python ../client/main.py " + cur_player.name +
+    std::string string_req = "python3.9 ../client/main.py " + cur_player.name +
                              " " + std::to_string(score) + " > " + file_name;
     const char *request = string_req.c_str();
     try {
